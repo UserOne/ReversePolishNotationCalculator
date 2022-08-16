65dc817ab10c5187f7000eabd4ff17e8a098c0cb
Умар Висаитов <100874991+UmarVis@users.noreply.github.com>
2022-08-16 10:52:36 +0300

    Разделение метода
    Здесь лучше использовать не один метод-тест, а несколько для каждого проверяемого сценария=)

--- src/test/java/ReversePolishNotationCalculatorTest.java
+++ src/test/java/ReversePolishNotationCalculatorTest.java
@@ -8,7 +8,12 @@ public class ReversePolishNotationCalculatorTest {
 
     @Test
     public void shouldCalculateAddition() {
+        //Например вместо: assertEquals(calc.calculatePolishNotation("3 4 +"), 7);
+        
+    public void shouldCaclulatePlus() {
         assertEquals(calc.calculatePolishNotation("3 4 +"), 7);
+    } // и так разбить каждый тест
+        
         assertEquals(calc.calculatePolishNotation("-3 -4 +"), -7);
         assertEquals(calc.calculatePolishNotation("1 2 3 - +"), 0);
         assertEquals(calc.calculatePolishNotation("3 4 *"), 12);
